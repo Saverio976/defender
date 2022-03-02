67c9f0f69e3312cbcb48a7c57b36a5452f757788
saverio <xavier.mitault@epitech.eu>
2022-03-02 12:28:53 +0100

    Free a any parse

--- src/init/init_menu.c
+++ src/init/init_menu.c
@@ -18,21 +18,21 @@ int init_level_menu(window_t *win)
     scene_t *scene = create_scene(win, sfBlack);
     object_t *background = NULL;
     any_t *any = parse_json_file(LEVEL_MENU);
+    any_t *any_dic = NULL;
 
     if (scene == NULL || any == NULL) {
         return RET_ERR_MALLOC;
     }
-    any = dico_t_get_any(any->value.dict, "background path");
+    any_dic = dico_t_get_any(any->value.dict, "background path");
     background = create_object(NULL, NULL, scene);
-    if (background == NULL) {
+    if (background == NULL || any_dic == NULL) {
         return RET_ERR_MALLOC;
     }
-    if (any != NULL) {
-        if (object_set_sprite(background, any->value.str,
-            (sfIntRect) {-1, -1, -1, -1}, (sfVector2f) {960, 540}) != BGS_OK) {
-            return RET_INVALID_INPUT;
-        }
+    if (object_set_sprite(background, any_dic->value.str,
+        (sfIntRect) {-1, -1, -1, -1}, (sfVector2f) {960, 540}) != BGS_OK) {
+        return RET_INVALID_INPUT;
     }
+    destroy_any(any);
     return create_button(scene, LEVEL_MENU);
 }
 
