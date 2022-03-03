8d8478f3a34aad12295f3a93ba2fbeebb6dde912
saverio <xavier.mitault@epitech.eu>
2022-03-03 21:25:41 +0100

    Add ennemy launch on map

--- src/init/create_ennemy.c
+++ src/init/create_ennemy.c
@@ -11,6 +11,29 @@
 #include "defender.h"
 #include "defender_ennemy.h"
 
+static sfVector2f get_pos_map_ennemy(scene_t *scene, int nb_spawn)
+{
+    char **map = NULL;
+    int nb_start = 0;
+    int is_found = 0;
+    sfVector2i pos = {0, 0};
+
+    map = dico_t_get_value(scene->components, SCENE_COMP_MAP);
+    if (map == NULL) {
+        return ((sfVector2f) {0, 0});
+    }
+    for (pos.y = 0; is_found == 0 && map[pos.y] != NULL; pos.y++) {
+        for (pos.x = 0; is_found == 0 && map[pos.y][pos.x] != '\0'; pos.x++) {
+            nb_start += (map[pos.y][pos.x] == MAP_SPAWN_CHAR) ? 1 : 0;
+            is_found = (nb_start == nb_spawn) ? 1 : 0;
+        }
+    }
+    if (is_found == 0) {
+        return ((sfVector2f) {0, 0});
+    }
+    return ((sfVector2f) {pos.x, pos.y});
+}
+
 static int fill_vector2i(any_t *json, sfIntRect *rect)
 {
     any_t *any = NULL;
@@ -34,12 +57,14 @@ static int fill_vector2i(any_t *json, sfIntRect *rect)
     return (RET_OK);
 }
 
-static int set_obj_ennemy(object_t *obj, any_t *json, sfVector2f pos)
+static int set_obj_ennemy(scene_t *scene, object_t *obj, any_t *json,
+        int nb_spawn)
 {
     any_t *any = NULL;
     char *path_sprite = NULL;
     sfIntRect rect = {-1, -1, -1, -1};
     int ret_code = 0;
+    sfVector2f pos = {0};
 
     any = get_from_any(json, "d", "path");
     if (any == NULL || any->type != STR) {
@@ -50,11 +75,12 @@ static int set_obj_ennemy(object_t *obj, any_t *json, sfVector2f pos)
     if (ret_code != RET_OK) {
         return (ret_code);
     }
+    pos = get_pos_map_ennemy(scene, nb_spawn);
     ret_code = object_set_sprite(obj, path_sprite, rect, pos);
     return (ret_code);
 }
 
-int create_ennemy(scene_t *scene, char const *path_json, sfVector2f pos)
+int create_ennemy(scene_t *scene, char const *path_json, int nb_spawn)
 {
     any_t *json = NULL;
     object_t *obj = NULL;
@@ -70,7 +96,7 @@ int create_ennemy(scene_t *scene, char const *path_json, sfVector2f pos)
     if (obj == NULL) {
         return (RET_ERR_MALLOC);
     }
-    set_obj_ennemy(obj, json, pos);
+    set_obj_ennemy(scene, obj, json, nb_spawn);
     destroy_any(json);
     return (RET_OK);
 }
