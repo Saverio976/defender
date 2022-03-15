93df99a0fa1c533661501a0a0443e2c7932e3daa
saverio <xavier.mitault@epitech.eu>
2022-03-16 00:21:51 +0100

    Fix follow ennemy

--- src/tower/shot_ennemy.c
+++ src/tower/shot_ennemy.c
@@ -5,28 +5,38 @@
 ** shot ennemy
 */
 
+#include <SFML/Graphics/Sprite.h>
 #include <math.h>
+#include <SFML/Graphics.h>
 #include "defender_game_data.h"
 
 static float pi = 3.1415926535;
 
 static float get_coef(sfFloatRect intersection, sfFloatRect tower)
 {
-    sfVector2f intersec_pos = {intersection.left + (intersection.width / 2),
-        intersection.top + (intersection.height / 2)};
-    sfVector2f tower_pos = {tower.left + (tower.width / 2),
-        tower.top + (tower.height / 2)};
-    float x = intersec_pos.x - tower_pos.x;
-    float y = intersec_pos.y - tower_pos.y;
+    float y1 = 0;
+    float y2 = 0;
+    float x1 = 0;
+    float x2 = 0;
+    double angle = 0;
 
-    return y / x;
+    x1 = tower.left + tower.width / 2;
+    y1 = tower.height + tower.height / 2;
+    x2 = intersection.left + intersection.width / 2;
+    y2 = intersection.top + intersection.height / 2;
+    angle = atan2(y2 - y1, x2 - x1) * 180 / pi;
+    return (angle);
 }
 
 void shot_ennemy(sfFloatRect intersection, object_t *tower)
 {
-    float coef = get_coef(intersection,
-        sfSprite_getGlobalBounds(tower->drawable.sprite));
+    float angle = 0;
 
-    sfSprite_setRotation(tower->drawable.sprite, (atan(coef) * 250 / pi));
+    if (tower == NULL) {
+        return;
+    }
+    angle = get_coef(intersection,
+            sfSprite_getGlobalBounds(tower->drawable.sprite));
+    sfSprite_setRotation(tower->drawable.sprite, angle + 90);
     return;
 }
