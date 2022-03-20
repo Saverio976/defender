dd4afd35a79307f5074cccaa510c45e94ccef412
saverio <xavier.mitault@epitech.eu>
2022-03-20 22:57:14 +0100

    Add -h and set icon

--- src/main.c
+++ src/main.c
@@ -6,16 +6,24 @@
 */
 
 #include "defender.h"
+#include "my_puts.h"
+#include "my_strings.h"
 #include "my_bgs.h"
 
 int main(int ac, char **av)
 {
-    window_t *win = init_defender(ac, av);
+    window_t *win = NULL;
 
+    if (ac == 2 && my_strcmp(av[1], "-h")) {
+        my_putstr(1, "./my_defender\n");
+        return (0);
+    }
+    win = init_defender(ac, av);
     if (win == NULL) {
         return (84);
     }
     window_set_framerate_limit(win, 60);
+    window_set_icon(win, "./assets/image/menu/setting/back.png");
     window_toglle_vsync(win);
     loop(win);
     remove_window(win);
